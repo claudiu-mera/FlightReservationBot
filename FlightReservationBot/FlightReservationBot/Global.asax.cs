992417e6c6cd896975d90cea87d0d2969c834f43
Claudiu Mera <claudiu.mera@endava.com>
2017-12-16 11:06:53 +0200

    Added CosmosDB support for state management

--- FlightReservationBot/FlightReservationBot/Global.asax.cs
+++ FlightReservationBot/FlightReservationBot/Global.asax.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using Autofac;
 using System.Web.Http;
-using System.Web.Routing;
+using System.Configuration;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
 
 namespace FlightReservationBot
 {
@@ -12,6 +14,25 @@ namespace FlightReservationBot
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var uri = new Uri(ConfigurationManager.AppSettings["DocumentDbUrl"]);
+            var key = ConfigurationManager.AppSettings["DocumentDbKey"];
+            var store = new DocumentDbBotDataStore(uri, key);
+
+            Conversation.UpdateContainer(
+                        builder =>
+                        {
+                            builder.Register(c => store)
+                                .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                                .AsSelf()
+                                .SingleInstance();
+
+                            builder.Register(c => new CachingBotDataStore(store, CachingBotDataStoreConsistencyPolicy.ETagBasedConsistency))
+                                .As<IBotDataStore<BotData>>()
+                                .AsSelf()
+                                .InstancePerLifetimeScope();
+
+                        });
         }
     }
 }
